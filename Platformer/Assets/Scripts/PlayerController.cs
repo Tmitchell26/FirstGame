3ac3c99f52128ab38bb8e7f5449bcc159f2594dc
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2020-02-15 13:41:32 +0800

    create player and floor

---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float turnSpeed = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            this.transform.position += this.transform.forward * Time.deltaTime * this.moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            this.transform.position -= this.transform.forward * Time.deltaTime * this.moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            this.transform.Rotate(this.transform.up, Time.deltaTime * -this.turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            this.transform.Rotate(this.transform.up, Time.deltaTime * this.turnSpeed);
+        }
+    }
+}
