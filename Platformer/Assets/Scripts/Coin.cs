4b717340ee465bf78186511ca43dbbef03179b61
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2020-02-17 20:38:55 +0800

    fixed scoring

--- Platformer/Assets/Scripts/Coin.cs
+++ Platformer/Assets/Scripts/Coin.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class Coin : MonoBehaviour
 {
     public float spinSpeed = 100f;
-    public int ScoreCount = 0;
-    public Text ScoreText;
+    //public int ScoreCount;
+    //public Text ScoreText;
 
     // Update is called once per frame
     void Update()
@@ -20,15 +20,15 @@ public class Coin : MonoBehaviour
         if(other.tag == "Character")
         {
             Destroy(this.gameObject);
-            AddScore();
+            //AddScore();
         }  
     }
 
-    void AddScore()
+    /*void AddScore()
     {
-        int Score = ScoreCount++;
+        ScoreCount++;
         ScoreText.text = ScoreCount.ToString();
-    }
+    }*/
 
 
 }
---
+++ Platformer/Assets/Scripts/Score.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int ScoreCount;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            AddScore();
+        }
+    }
+
+    void AddScore()
+    {
+        ScoreCount++;
+        ScoreText.text = ScoreCount.ToString();
+    }
+}
